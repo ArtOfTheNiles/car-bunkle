d1140fb0e108c9a313e42d200d3f16fe1a1f1c90
Niles Bontrager <nbontrager@porterone.local>
2025-04-08 17:40:51 -0400

    Wheel functionality added to motorbike cli

--- src/classes/Cli.ts
+++ src/classes/Cli.ts
@@ -248,7 +248,15 @@ class Cli {
           parseInt(answers.year),
           parseInt(answers.weight),
           parseInt(answers.topSpeed),
-          [],
+          [
+            new Wheel(
+              parseInt(answers.frontWheelDiameter), 
+              answers.frontWheelBrand
+          ), 
+            new Wheel(
+              parseInt(answers.rearWheelDiameter), 
+              answers.rearWheelBrand
+          )],
         );
         this.vehicles.push(motorbike);
         this.selectedVehicleVin = motorbike.vin;
